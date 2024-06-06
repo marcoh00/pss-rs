4b3bbb5a73a20acf4d9ab7d1ea04d19f09885641
Marco Hünseler <marco.huenseler@h-brs.de>
2024-06-06 18:54:54 +0200

    [group] cleanup uses

--- src/group.rs
+++ src/group.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashSet, marker::PhantomData};
+use std::marker::PhantomData;
 
-use crypto_bigint::{const_residue, generic_array::GenericArray, impl_modulus, modular::{constant_mod::{Residue, ResidueParams}, Retrieve}, rand_core::OsRng, ArrayDecoding, ArrayEncoding, CheckedSub, ConcatMixed, Encoding, NonZero, RandomMod, SplitMixed, SubMod, Uint, U2048, U256, U64};
+use crypto_bigint::{const_residue, generic_array::GenericArray, impl_modulus, modular::constant_mod::{Residue, ResidueParams}, rand_core::OsRng, ArrayDecoding, CheckedSub, ConcatMixed, Encoding, NonZero, RandomMod, SplitMixed, Uint, U2048, U64};
 use sha3::{digest::OutputSizeUser, Digest};
 
 const DH_MODP_2048_MODULUS_HEX: &str = "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF";
