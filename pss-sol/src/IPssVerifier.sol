3b2e8ce45a69a6ccb77b8c098bcaf3fe66f2f3a9
Marco Hünseler <marco.huenseler@h-brs.de>
2024-11-14 13:07:47 +0100

    [sol] fix PSS interface to represent the use of ecc.point and unify retrieval of gpk/sector keys

--- pss-sol/src/IPssVerifier.sol
+++ pss-sol/src/IPssVerifier.sol
@@ -17,4 +17,6 @@ interface IPssVerifier {
     function validate_signature_p1(bytes calldata message, uint256 c, uint256 s1, uint256 s2, ECC.Point memory i_sector_icc_1) external view returns (bool);
     function validate_signature_p2(bytes calldata message, uint256 c, uint256 s1, uint256 s2, ECC.Point memory i_sector_icc_2) external view returns (bool);
     function validate_signature_p1_p2(bytes calldata message, uint256 c, uint256 s1, uint256 s2, ECC.Point memory i_sector_icc_1, ECC.Point memory i_sector_icc_2) external view returns (bool);
+    function get_gpk() external view returns (uint256, uint256, uint256, uint256);
+    function get_sector() external view returns (uint256, uint256);
 }
